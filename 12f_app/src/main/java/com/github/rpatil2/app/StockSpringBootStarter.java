ee6fc10fbc3af1648b045e0e828e217db581f096
Ritika Patil <Ritika.Patil@sas.com>
2017-03-29 15:33:08 -0400

    Add Redis service

--- 12f_app/src/main/java/com/github/rpatil2/app/StockSpringBootStarter.java
+++ 12f_app/src/main/java/com/github/rpatil2/app/StockSpringBootStarter.java
@@ -2,7 +2,9 @@ package com.github.rpatil2.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @SpringBootApplication
 public class StockSpringBootStarter {
     public static void main(String[] args) {
