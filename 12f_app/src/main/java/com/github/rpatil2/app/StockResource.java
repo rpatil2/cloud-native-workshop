8044334c43e87bcf077a1c98ea4e359c9906ecb9
Ritika Patil <Ritika.Patil@sas.com>
2017-03-30 16:53:40 -0400

    Config server files added

--- 12f_app/src/main/java/com/github/rpatil2/app/StockResource.java
+++ 12f_app/src/main/java/com/github/rpatil2/app/StockResource.java
@@ -11,6 +11,8 @@ import com.github.rpatil2.app.domain.model.StockItem;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -24,7 +26,7 @@ public class StockResource {
 	@Autowired
 	private StockItemRepository stockItemRepository;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+/*	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
 
 	    StockItem stockItem = stockItemRepository.findOne(id);
@@ -34,6 +36,23 @@ public class StockResource {
 	        return null;
 	    }
 
+	    return stockItem;
+	}*/
+	
+	private static final Logger logger = LoggerFactory.getLogger(StockResource.class);
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
+	    logger.info("Starting search of stock item(id={}) search", id);
+
+	    StockItem stockItem = stockItemRepository.findOne(id);
+
+	    if (stockItem == null) {
+	        logger.info("Stock item(id={}) has not been found", id);
+	        response.setStatus(HttpStatus.NOT_FOUND.value());
+	        return null;
+	    }
+
+	    logger.info("Finishing search of stock item(id={}) search", id);
 	    return stockItem;
 	}
 
